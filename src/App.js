bedc051b81f4beb64302c4dd62bc1210b65309f7
gustav-mani <gustavvm123@gmail.com>
2021-04-01 16:53:22 +0530

    toggled off animation

--- src/App.js
+++ src/App.js
@@ -11,10 +11,9 @@ import Fade from "react-reveal/Fade";
 import Slide from "react-reveal/Slide";
 import ContactForm from "./pages/contact-form/contact-form.component";
 import { Parallax } from "react-parallax";
-import Particles from "react-particles-js";
-import { particlesOptions } from "./particlesOptions";
+/* import Particles from "react-particles-js";
+import { particlesOptions } from "./particlesOptions"; */
 import FooterPanel from "./components/footer/footer.component";
-
 import "./App.css";
 
 const App = () => {
@@ -23,11 +22,10 @@ const App = () => {
       <MyCarousal />
       <MyTitleMessage />
       <MyNavbar />
-      <Particles
-        className="particles particles-box"
-        params={particlesOptions}
-      /> 
-
+      {/* <Particles
+      className="particles particles-box"
+      params={particlesOptions}
+      />  */}
       <div>
         <Parallax
           blur={{ min: -30, max: 30 }}
